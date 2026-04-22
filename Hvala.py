1a0c760b5e7679397b93870f69b2c98bc7067458
cirickenan-beep <cirickenan@gmail.com>
2026-04-22 12:34:39 +0200

    Add files via upload

---
+++ Hvala.py
@@ -0,0 +1,21 @@
+import streamlit as st
+st.markdown(
+ """
+ <style>
+ .stApp {
+     background-color: #ADD8E6;
+     }
+     </style>
+     """,
+    unsafe_allow_html=True
+)
+
+st.title("Babo ovo je za tebe")
+
+
+if st.button("Pritisni da nastaviš"):
+ st.balloons()
+ st.write("Puno puta si mi govorio kako moram Pc koristi za korisne stvari.Na poèetku to nisam shaæao ozbiljno ali kodiranje je to sve promjenilo")
+ st.write("Hvala ti na svemu")
+ st.write("Voli te tvoj gamer")
+ 
